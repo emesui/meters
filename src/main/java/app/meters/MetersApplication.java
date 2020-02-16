006c82b097996cb9acddb10af5a9db28d937953a
Michal Stolarczyk <michal.stolarczyk@allegro.pl>
2020-02-16 21:55:02 +0100

    spring init

---
+++ src/main/java/app/meters/MetersApplication.java
@@ -0,0 +1,13 @@
+package app.meters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetersApplication.class, args);
+	}
+
+}
